a60276a64a38d8862b47f1434a14dfa66c76e01a
surabhi <surabhi.das@outlook.com>
2023-07-29 17:09:07 -0400

    starting project

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,6 +1,8 @@
 public class Simulation {
     private final Vehicle vehicle;
 
+
+
     public Simulation(Vehicle v) {
         this.vehicle = v;
     }
@@ -73,9 +75,9 @@ public class Simulation {
     }
 
     public static void main(String[] args) {
-        // create a new Simulation object with a random starting altitude
-        // create a new BurnInputStream
-        // pass the new BurnInputStream to the runSimulation method
+        Simulation game = new Simulation(new Vehicle(Simulation.randomaltitude()));
+        BurnStream burnSource = new BurnInputStream();
+        game.runSimulation(burnSource);
     }
 
 }
